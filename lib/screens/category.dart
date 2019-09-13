1a69758e0620b526225a2b75c30f535187cb6994
piyawat <piyawat@gmail.com>
2019-09-13 14:39:49 +0700

    Finish 13Sep

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -8,26 +8,43 @@ class Category extends StatefulWidget {
 }
 
 class _CategoryState extends State<Category> {
-
 // Explicit
 
+String categoryString = 'อาหารจานด่วน';
 
 // Method
-@override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
     readFireStore();
   }
 
-  Future<void> readFireStore() async{
-
+  Future<void> readFireStore() async {
+    Firestore firestore = Firestore.instance;
+    await firestore
+        .collection('Shop')
+        .document('HappyTeaTimes')
+        .collection('Menu_Types')
+        .document(categoryString)
+        .collection('Menu_Items')
+        .snapshots()
+        .listen((response) {
+      List<DocumentSnapshot> snapshots = response.documents;
+      for (var snapshot in snapshots) {
+        String nameDocument = snapshot.documentID;
+        print('nameDocument = $nameDocument');
+        String nameTitle = snapshot.data['NameFood'];
+        print('nameTitle = $nameTitle');
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),body: Text('body'),
+      appBar: AppBar(),
+      body: Text('body'),
     );
   }
 }
