eac87b7c09611a1a9c27ce02e40644ee99da717e
piyawat <piyawat@gmail.com>
2019-11-22 14:38:33 +0700

    List Product Ok

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shop_bakerclick/models/menu_item_model.dart';
+import 'package:shop_bakerclick/utility/my_style.dart';
 
 class Category extends StatefulWidget {
   final String myCategory;
@@ -51,20 +52,70 @@ class _CategoryState extends State<Category> {
     });
   }
 
-  Widget showName(int index){
-    return Text(menuItemModels[index].nameFood);
+  Widget showName(int index) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          menuItemModels[index].nameFood,
+          style: index%2 == 0 ? Mystye().h2MainTextStyle : Mystye().h2WeakTextStyle
+        ),
+      ],
+    );
+  }
+
+  Widget showDetail(int index) {
+    String detail = menuItemModels[index].detail;
+    if (detail.length >= 40) {
+      detail = detail.substring(0, 39);
+      detail = '$detail ...';
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+            width: MediaQuery.of(context).size.width * 0.5 - 10.0,
+            child: Text(
+              detail,
+              style: TextStyle(
+                  color: index % 2 == 0 ? Colors.black : Colors.white),
+            )),
+      ],
+    );
+  }
+
+  Widget showPrice(int index) {
+    String price = menuItemModels[index].price;
+    price = '$price thb';
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Text(
+          price,
+          style: Mystye().h1TextStyle,
+        ),
+      ],
+    );
   }
 
   Widget showText(int index) {
     return Container(
+      padding: EdgeInsets.only(right: 10.0),
       width: MediaQuery.of(context).size.width * 0.5,
       height: MediaQuery.of(context).size.width * 0.4,
-      child: Column(children: <Widget>[showName(index),],),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          showName(index),
+          showDetail(index),
+          showPrice(index),
+        ],
+      ),
     );
   }
 
   Widget showPictrue(int index) {
-    return Container( 
+    return Container(
       padding: EdgeInsets.all(16.0),
       width: MediaQuery.of(context).size.width * 0.5,
       height: MediaQuery.of(context).size.width * 0.4,
@@ -76,8 +127,13 @@ class _CategoryState extends State<Category> {
   }
 
   Widget showList(int index) {
-    return Row(
-      children: <Widget>[showPictrue(index), showText(index)],
+    return Container(
+      decoration: index % 2 == 0
+          ? BoxDecoration(color: Mystye().wakeColor)
+          : BoxDecoration(color: Mystye().mainColor),
+      child: Row(
+        children: <Widget>[showPictrue(index), showText(index)],
+      ),
     );
   }
 
---
+++ lib/utility/my_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Mystye {
+
+  Color mainColor = Colors.green.shade700;
+  Color wakeColor = Colors.green.shade200;
+
+
+
+  TextStyle h2MainTextStyle = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.blue.shade800,
+  );
+
+  TextStyle h2WeakTextStyle = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.yellow.shade600,
+  );
+
+  TextStyle h1TextStyle = TextStyle(
+    fontSize: 30.0,
+    fontStyle: FontStyle.italic,
+    color: Colors.pink,
+  );
+
+  Mystye();
+}
